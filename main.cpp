d3eee466e0a479e70d3ae0aedd5374ed0e2fa50c
JFergen <josephfergen150@gmail.com>
2019-04-17 10:34:04 -0500

    Finished milestone

--- main.cpp
+++ main.cpp
@@ -1,18 +1,43 @@
-#include "Graph.h"
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-msc30-c"
+#include <iostream>
+#include <string>
+#include <list>
+#include <cctype>
 #include <fstream>
-#include <vector>
 #include <sstream>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+string getSynonym(string key, int rand);
+
+map<string, set<string>> totalGraph;
 
 int main()
 {
-    vector<Graph> GraphList; // Holds the entirety of the graph data
-    int numWord = 0; // Holds the number of words that have synonyms
+    int nTimes = 0;
     string fileName;
     ifstream fin; // Used to parse through file
     string firstWord; // This is the first word of every line (the main vertices)
+    string vertKey;
     string otherWords; // Every word past the ':' (the synonyms)
+    set<string>::iterator removeThis;
+    int userChoice;
+    string paragraphWords; // Used to read each word in paragraph
+    int userRandTimes; // Number of times the user wants to hop randomly
+    char puncChar = 0; // Used to see if final character is punctuation character
+    bool capLetter; // Used to see if the first letter is capital or not
+    bool hasPunc;
     
-    cout << "Enter filename containing tables and data: ";  // Gets filename to open from user
+    srand(time(nullptr));
+    rand();
+    
+    cout << "Enter filename containing words and synonyms: ";  // Gets filename to open from user
     cin >> fileName;
     
     fin.open(fileName.c_str()); // Opens main file for input
@@ -25,22 +50,185 @@ int main()
     // Reads through all the starting data and puts it into the graph
     while(getline(fin, firstWord, ':'))
     {
-        Graph dataEntry;
-        dataEntry.SetStartWord(firstWord); // Sets the first word for each graph entry
-        
-        getline(fin, otherWords); // Gets rest of line (including ',')
+        set<string> bigSet;
+        set<string> smallSet;
+        bigSet.insert(firstWord);
     
-        // Used to get rid of commas
-        istringstream commasGoAway(otherWords);
+        getline(fin, otherWords);
+        istringstream commasGoAway(otherWords); // Used to get rid of commas
+    
         while(getline(commasGoAway, otherWords, ','))
         {
-            dataEntry.NextSynWord(otherWords);
+            bigSet.insert(otherWords);
         }
         
-        GraphList.push_back(dataEntry);
-        ++numWord;
+        for(auto it = bigSet.begin(); it != bigSet.end(); it++)
+        {
+            smallSet.clear();
+            
+            if(totalGraph.find(*it) == totalGraph.end())
+            {// not found in map
+                vertKey = *it; // Key of the map
+                smallSet = bigSet; // smallSet is used for deleting the key from the set before inserting
+    
+                // Removes the key from the set
+                for(auto itr = smallSet.begin(); itr != smallSet.end(); itr++)
+                {
+                    if(*itr == vertKey)
+                    {
+                        removeThis = itr;
+                    }
+                }
+                smallSet.erase(removeThis);
+                
+                totalGraph.insert(pair<string, set<string>>(vertKey, smallSet));
+            } else
+            { // found in the map
+                vertKey = *it;
+                
+                set_union(totalGraph[*it].begin(), totalGraph[*it].end(), bigSet.begin(), bigSet.end(),
+                        inserter(smallSet, smallSet.begin())); // Combines the old set with new set
+    
+                //Removes key from set
+                for(auto itr = smallSet.begin(); itr != smallSet.end(); itr++)
+                {
+                    if(*itr == vertKey)
+                    {
+                        removeThis = itr;
+                    }
+                }
+                smallSet.erase(removeThis);
+                
+                totalGraph[*it] = smallSet; // Setting the set to be the combined set
+            }
+        }
+    }
+    fin.close();
+    
+    // Getting user choice for what to do
+    cout << "1 for random synonym replacement. 2 for performing a paragraph similarity analysis." << endl;
+    cin >> userChoice;
+    cin.ignore();
+    
+    while(userChoice != 2 && userChoice != 1) // Making sure user chooses 1 or 2
+    {
+        cout << "Invalid choice. Try again." << endl <<
+            "1 for random synonym replacement. 2 for performing a paragraph similarity analysis." << endl;
+        cin >> userChoice;
     }
     
-    fin.close();
+    switch(userChoice)
+    {
+        case 1: // Random synonym replacement
+            cout << "Enter filename containing paragraph for replacement: ";
+            cin >> fileName;
+        
+            fin.open(fileName.c_str()); // Opens main file for input
+            if(!fin.is_open())
+            {
+                cout << fileName <<  " could not be found";
+                return EXIT_FAILURE;
+            }
+            
+            userRandTimes = 1; //TODO Make random # of changes based on user's input (must be >= 1) (1 for milestone)
+        
+            while(getline(fin, paragraphWords, ' '))
+            {
+                capLetter = false;
+                hasPunc = false;
+                
+                // If the last character is punctuation
+                if(ispunct(paragraphWords.back()))
+                {
+                    hasPunc = true;
+                    puncChar = paragraphWords.back(); // Gets last character of each string (to check for punctuation)
+                    paragraphWords.pop_back();
+                }
+                
+                if(totalGraph.find(paragraphWords) == totalGraph.end()) // If I am not changing the word, then just print it out
+                {// Not found in map
+                    
+                    if(hasPunc)
+                    {
+                        paragraphWords += puncChar;
+                    }
+                    
+                    if(nTimes == 0) // If it is the first word of the paragraph;
+                    {
+                        cout << paragraphWords;
+                        nTimes++;
+                        continue;
+                    }
+                    cout << " " << paragraphWords;
+                    continue;
+                }
+                else // If it is in the map
+                {
+                    // If the first character is a capital letter
+                    if(isupper(paragraphWords.front()))
+                    {
+                        capLetter = true;
+                    }
+    
+                    paragraphWords = getSynonym(paragraphWords, userRandTimes); // Gets the synonym according to # of hops
+    
+                    // Change the first letter of synonym to capital if need be
+                    if(capLetter)
+                    {
+                        toupper(paragraphWords[0]);
+                    }
+                    
+                    if(hasPunc)
+                    {
+                        paragraphWords += puncChar;
+                    }
+                    
+                    if(nTimes == 0)
+                    {
+                        cout << paragraphWords;
+                        nTimes++;
+                        continue;
+                    }
+                    else
+                    {
+                        cout << " " << paragraphWords;
+                        continue;
+                    }
+                }
+            }
+            break;
+        case 2: // Paragraph similarity analysis//TODO
+            cout << "Not yet implemented" << endl;
+            break;
+        default:
+            cout << "Please don't go here somehow";
+            return 1;
+    }
+    
     return 0;
 }
+
+string getSynonym(string key, int randTimes)
+{
+    int randWordNum = 0;
+    int i = 1;
+    
+    do
+    {
+        randWordNum = rand() % totalGraph[key].size(); //
+        
+        if(i == randTimes)
+        {
+            auto it = totalGraph[key].begin();
+            advance(it, randWordNum);
+            return *it;
+        }
+        
+        auto itr = totalGraph[key].begin();
+        advance(itr, randWordNum);
+        key = *itr;
+        i++;
+    }while(i != randTimes);
+}
+
+#pragma clang diagnostic pop
